3549496f6fc472e9deea9a34a02829e9294d3da0
eyesless1 <yuriygovorov1607@gmail.com>
2026-04-23 00:26:37 +0300

    lp_3.2

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -12,13 +12,17 @@
                 prepend-icon="mdi-account"
                 name="email"
                 label="Email"
-                type="email">
+                type="email"
+                v-model="email"
+              >
               </v-text-field>
               <v-text-field
                 prepend-icon="mdi-lock"
                 name="password"
                 label="Password"
-                type="password">
+                type="password"
+                v-model="password"
+              >
               </v-text-field>
             </v-form>
           </v-card-text>
@@ -36,6 +40,8 @@
 export default {
   data () {
     return {
+      email: "",
+      password: ""
     }
   }
 }
