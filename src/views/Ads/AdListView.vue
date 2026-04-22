43e74ca113267e359b375d3a77a8870098526f03
eyesless1 <yuriygovorov1607@gmail.com>
2026-04-23 00:59:01 +0300

    lp_8

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,5 +1,84 @@
 <template>
-  <div class="about">
-    <h1>Список объявлений (AdListView)</h1>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" lg="6">
+        <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+        
+        <v-card 
+          v-for="ad in ads" 
+          :key="ad.id" 
+          class="mb-3" 
+          max-width="1000"
+        >
+          <v-row>
+            <!-- Блок картинки - 4 колонки -->
+            <v-col cols="4">
+              <v-img 
+                :src="ad.src" 
+                height="175px" 
+                cover
+              ></v-img>
+            </v-col>
+            
+            <!-- Блок текста и кнопки - 8 колонок -->
+            <v-col cols="8">
+              <h2 class="text--primary">{{ ad.title }}</h2>
+              <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">
+                {{ ad.desc }}
+              </p>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                  color="primary" 
+                  variant="text" 
+                  :to="'/ad/' + ad.id"
+                >
+                  Open
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      ads: [
+        {
+          title: "First",
+          desc: "First Desc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "1"
+        },
+        {
+          title: "Second",
+          desc: "Second Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          id: "2"
+        },
+        {
+          title: "Third",
+          desc: "Third Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          id: "3"
+        },
+        {
+          title: "Fourth",
+          desc: "Fourth Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          id: "4"
+        }
+      ]
+    }
+  }
+}
+</script>
