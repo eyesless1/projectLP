cc15be24303cfd2d5d7c1b7859ca5a1fc0a204b3
eyesless1 <yuriygovorov1607@gmail.com>
2026-04-23 01:22:54 +0300

    lp_11

--- src/store/ads.js
+++ src/store/ads.js
@@ -31,6 +31,17 @@ export default {
       }
     ]
   },
+  mutations: {
+    createAd(state, payload) {
+      state.ads.push(payload)
+    }
+  },
+  actions: {
+    createAd({ commit }, payload) {
+      payload.id = Math.random()
+      commit('createAd', payload)
+    }
+  },
   getters: {
     ads(state) {
       return state.ads
@@ -44,7 +55,5 @@ export default {
     adById(state) {
       return id => state.ads.find(ad => ad.id == id)
     }
-  },
-  mutations: {},
-  actions: {}
+  }
 }
--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -3,12 +3,7 @@
     <v-row>
       <v-col cols="8" offset="2">
         <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col cols="8" offset="2">
-        <v-form v-model="valid" ref="form">
+        <v-form v-model="valid" ref="form" validation>
           <v-text-field
             name="title"
             label="Ad Title"
@@ -17,7 +12,6 @@
             :rules="[v => !!v || 'Title is required']"
           >
           </v-text-field>
-          
           <v-textarea
             name="description"
             label="Ad Description"
@@ -27,49 +21,40 @@
             class="mb-3"
           ></v-textarea>
         </v-form>
-      </v-col>
-    </v-row>
 
-    <!-- Блок 1: Кнопка загрузки картинки -->
-    <v-row>
-      <v-col cols="8" offset="2">
-        <v-btn class="mt-3" color="warning">
-          Upload
-          <v-icon right dark>mdi-cloud-upload</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
+        <v-layout row>
+          <v-flex xs12>
+            <v-btn class="mt-3" color="warning">
+              Upload
+              <v-icon right dark>mdi-cloud-upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
 
-    <!-- Блок 2: Превью картинки -->
-    <v-row>
-      <v-col cols="8" offset="2">
-        <img
-          src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
-          height="150"
-          class="mt-3"
-        />
-      </v-col>
-    </v-row>
+        <v-layout row>
+          <v-flex xs12>
+            <img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="150" class="mt-3">
+          </v-flex>
+        </v-layout>
 
-    <!-- Блок 3: Переключатель Promo -->
-    <v-row>
-      <v-col cols="8" offset="2">
-        <v-switch v-model="promo" label="Ad to Promo?"></v-switch>
-      </v-col>
-    </v-row>
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch
+              v-model="promo"
+              label="Ad to Promo?"
+            ></v-switch>
+          </v-flex>
+        </v-layout>
 
-    <!-- Блок 4: Кнопка создания объявления -->
-    <v-row>
-      <v-col cols="8" offset="2">
-        <v-spacer></v-spacer>
-        <v-btn
-          color="success"
-          @click="createAd"
-          :loading="loading"
-          :disabled="!valid || loading"
-        >
-          Create Ad
-        </v-btn>
+        <v-layout row>
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="success"
+              @click="createAd"
+            >Create Ad</v-btn>
+          </v-flex>
+        </v-layout>
       </v-col>
     </v-row>
   </v-container>
@@ -82,8 +67,7 @@ export default {
       valid: false,
       title: "",
       description: "",
-      promo: true,
-      loading: false
+      promo: false
     }
   },
   methods: {
@@ -92,9 +76,17 @@ export default {
         const ad = {
           title: this.title,
           desc: this.description,
-          promo: this.promo
+          promo: this.promo,
+          src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
         }
-        console.log(ad)
+        this.$store.dispatch("createAd", ad)
+        // Очистка формы после создания
+        this.title = ""
+        this.description = ""
+        this.promo = false
+        this.$refs.form.reset()
+        // Переход на главную
+        this.$router.push("/")
       }
     }
   }
